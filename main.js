57a3cc96333266ae4f9f66036e9f7276590a5b08
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2022-02-13 20:51:16 +0300

    refactor: Les05-06

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 // less01
-setInterval(function (){
+setInterval(function () {
     const message = document.getElementById("message");
     console.log(message.value)
 }, 4000)
+
+//less06
+// window.alert("Hey");
+// window.alert("Bye")
+
+//les07
+
+let str1;
+str1 = "number1";
+// window.alert(str1)
+
+let wordCat;
+wordCat = "cat";
+
+let translateOfCat = "Кот"
+// window.alert(wordCat);
+// window.alert(translateOfCat)
