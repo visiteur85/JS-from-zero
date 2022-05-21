8e5778bd11dbbc0d353c823595d33037f2ed3736
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2022-05-21 19:15:16 +0300

    add Less 14-11

--- main.js
+++ main.js
@@ -621,7 +621,27 @@
 //     thumbnails[i].addEventListener('click', showFullSizeImage);
 // }
 //------------------------------14-10----------------------------
-
+// // 14-11
+//
+// var contextMenu = document.getElementById('context-menu');
+//
+// var contextableArea = document.getElementById('area-with-context-menu');
+//
+// function hideContextMenu(e){
+//     contextMenu.classList.add('hidden');
+// }
+//
+// function showContextMenu(e) {
+//     //отменяет стандартное действие
+//     e.preventDefault();
+//     contextMenu.classList.remove('hidden');
+//
+//     // ЗДЕСЬ ПИШЕМ СВОЙ КОД!!!!
+//     //помним, что в объекте ивента e содержится много пропертей, среди которых и св-ва с координатами клика
+// };
+//
+// contextableArea.addEventListener('contextmenu', showContextMenu);
+// contextableArea.addEventListener('click', hideContextMenu);
 
 
 let menuItemsLabelArr = document.querySelectorAll('.main-menu-item span');
