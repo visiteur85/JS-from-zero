0e6492f7d0a7fa5c336532be1e4d8b9ba563a892
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2022-05-24 22:23:52 +0300

    normal calc 1

--- math.js
+++ math.js
@@ -1,54 +1,62 @@
- const sum = (a, b) => {
+const sum = (a, b) => {
     return a + b
 }
- const multiply = (a, b) => {
+const multiply = (a, b) => {
     return a * b
 }
- const minus = (a, b) => {
+const minus = (a, b) => {
     return a - b
 }
- const divide = (a, b) => {
+const divide = (a, b) => {
     return a / b
 }
 
-let resultClc =   document.querySelector("#result")
+let resultClc = document.querySelector("#result")
 
 //забираем айди  инпутов
 let numberOne = document.querySelector("#numberOne");
 let numberTwo = document.querySelector("#numberTwo");
-//забираем значение импутов
-let numberOneValue = +numberOne.value;
-let numberTwoValue = +numberTwo.value;
-//забираем айди кнопок
- let buttonSum = document.querySelector("#sum");
- let buttonMinus = document.querySelector("#minus");
- let buttonMult = document.querySelector("#mult");
- let buttonDev = document.querySelector("#dev");
-
- const sumHandler = () => {
-     let result = sum(numberOneValue, numberTwoValue);
-     resultClc.innerHTML = result
- };
- const minusHandler = () => {
-     let result = minus(numberOneValue, numberTwoValue);
-     resultClc.innerHTML = result
- };
- const mulHandler = () => {
-     let result = multiply(numberOneValue, numberTwoValue);
-     resultClc.innerHTML = result
- };
- const divideHandler = () => {
-     let result = divide(numberOneValue, numberTwoValue);
-     resultClc.innerHTML = result
- };
 
 
+const arrButtons = document.querySelectorAll(".operation")
+
+let resultOfOperation = (math) => {
+    let numberOneValue = +numberOne.value;
+    let numberTwoValue = +numberTwo.value;
+
+    if (math === "+") {
+        let result = sum(numberOneValue, numberTwoValue);
+        resultClc.innerHTML = result
+    }
+    if (math === "-") {
+        let result = minus(numberOneValue, numberTwoValue);
+        resultClc.innerHTML = result
+    }
+    if (math === "*") {
+        let result = multiply(numberOneValue, numberTwoValue);
+        resultClc.innerHTML = result
+    }
+    if (math === "/") {
+        let result = divide(numberOneValue, numberTwoValue);
+        resultClc.innerHTML = result
+    }
+}
+
+const operation = (even) => {
+    let button = even.currentTarget;
+    let buttonValue = button.innerText
+    resultOfOperation(buttonValue)
+
+}
+
+for (let i = 0; i < arrButtons.length; i++) {
+    arrButtons[i].addEventListener("click", operation)
+}
 
 
- buttonSum.addEventListener("click", sumHandler );
- buttonMinus.addEventListener("click", minusHandler );
- buttonMult.addEventListener("click", mulHandler );
- buttonDev.addEventListener("click", divideHandler );
+
+
+
 
 
 
